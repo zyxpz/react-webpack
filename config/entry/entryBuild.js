9055f8b6c90ff632472dccc06aa5bd9354c61baa
wb-mxl231031 <wb-mxl231031@cainiao.com>
2018-04-16 15:33:17 +0800

    自动化生成多入口文件

---
+++ config/entry/entryBuild.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const entry = require('./entry');
+
+/**
+ * 节点的深度删除模块
+ */
+const rimraf = require('rimraf');
+
+/**
+ * 定义entryBuild
+ */
+const entryBuildPath = path.resolve(__dirname, '../../entryBuild');
+
+/**
+ * 删除entryBuild
+ */
+rimraf.sync(entryBuildPath);
+
+/**
+ * 创建entryBuild
+ */
+fs.mkdirSync(entryBuildPath);
+
+const entyrContent = data => `
+  import React from 'react'
+  import { render } from 'react-dom'
+  import Index from '../app/component/${data.path}'
+  import Header from '../app/component/common/Header';
+  import Footer from '../app/component/common/Footer';
+  render([<Header key="Header"/>,<Index key="Index"/>,<Footer key="Footer"/>],document.getElementById('app'))
+`
+
+entry.map(data => {
+  fs.writeFile(`${entryBuildPath}/${data.name}.js`, entyrContent(data), 'utf8', err => {
+    if (err) {
+      return console.log(ree);
+    }
+  })
+})
--- entryBuild/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Init from '../app/component/index/index';
-
-render(
-  <Init />,
-  document.getElementById('app')
-)
